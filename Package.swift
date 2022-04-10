77359ea01fe6a7da4ab0d7fa27c9673a1007ad48
Danny Sung <danny@dannysung.com>
2022-04-10 14:08:04 -0700

    Update Kitura package dependency

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["Koba"])
     ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMajor(from: "2.1.0"))
+        .package(url: "https://github.com/Kitura/Kitura.git", .upToNextMajor(from: "2.9.200"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
